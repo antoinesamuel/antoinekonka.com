b23a8ecff73b9ed106cd0094a07fa908e9474172
antoine KONKA <antoniosladetail@gmail.com>
2026-08-21 23:10:54 +0000

    Correction du Texte de la Home Page

--- app/Pages/HomePage.tsx
+++ app/Pages/HomePage.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <div className="h-screen w-full flex flex-col justify-center items-center bg-black relative">
       <FixedRotateText />
       {/* Contenu textuel central inspiré de ton design Figma */}
-      <div className="h-3/4 z-10 text-center pointer-events-none select-none flex flex-col items-center justify-end gap-4">
+      <div className="h-3/4 z-10 text-center pointer-events-none select-none flex flex-col items-center justify-center md:justify-end gap-4">
         <h1 className="text-white font-bold text-[8rem] md:text-[10rem] lg:text-[14rem] tracking-tight leading-none">
           Ciao, i{"'"}m
           <br />
